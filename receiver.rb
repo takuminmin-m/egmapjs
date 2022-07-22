d555e3684698ad97d2cdeff3b4459719734753e6
takuminmin-m <takuminmin0718@gmail.com>
2022-07-22 21:51:47 +0900

    commit

--- receiver.rb
+++ receiver.rb
@@ -5,11 +5,22 @@ require "net/http"
 lat = 36.12496349011065
 lng = 136.2212399754632
 
+sp = SerialPort.new("/dev/ttyUSB1", 9600)
+
+line = ""
 loop do
-  uri = URI("http://localhost:4567/send_coordinate/#{lat},#{lng}")
-  res = Net::HTTP.get_response(uri)
-  puts res.code
-  sleep(1)
-  lat += 0.0001
-  lng += 0.0001
+  chars = sp.gets
+  line += chars if chars
+  if line&.index("\r")
+    uri = URI("http://localhost:4567/send_coordinate/#{line.chomp}")
+    res = Net::HTTP.get_response(uri)
+    p line
+    line = ""
+  end
+  # sleep(1)
+
+  # puts res.code
+  # sleep(1)
+  # lat += 0.0001
+  # lng += 0.0001
 end
