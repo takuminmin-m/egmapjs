bc7d96da18c8e16d76c800aab8569e271868f9e8
takuminmin-m <takuminmin0718@gmail.com>
2022-09-15 18:39:22 +0900

    本番7-15

--- receiver.rb
+++ receiver.rb
@@ -5,15 +5,17 @@ require "net/http"
 lat = 36.12496349011065
 lng = 136.2212399754632
 
-sp = SerialPort.new("/dev/ttyUSB1", 9600)
+sp = SerialPort.new("/dev/ttyUSB0", 9600)
 
 line = ""
 loop do
   chars = sp.gets
   line += chars if chars
   if line&.index("\r")
-    uri = URI("http://localhost:4567/send_coordinate/#{line.chomp}")
-    res = Net::HTTP.get_response(uri)
+    begin
+      uri = URI("http://localhost:4567/send_coordinate/#{line.chomp}")
+      res = Net::HTTP.get_response(uri)
+    end
     p line
     line = ""
   end
